3054564e626289241752c36ce776cc7361e39235
lyd <lyddream@126.com>
2017-10-26 18:36:18 +0800

    complete tokenBalance,tokenAllowance, tokenTransfer and tokenApprovl

--- src/libs/src/relay.js
+++ src/libs/src/relay.js
@@ -6,7 +6,7 @@ const ethUtil = require('ethereumjs-util');
 const signer = require('./signer.js');
 const Joi = require('joi');
 const BigNumber = require('bignumber.js');
-
+const _ = require('lodash');
 
 function relay(host) {
 
@@ -83,18 +83,39 @@ function relay(host) {
             if (res.error) {
                 throw new Error(res.error);
             }
-            return new BigNumber(Number(res.result));
+            return new BigNumber(Number(validHex(res.result)));
         });
     };
 
-    this.getTokenBalance = async function (add, contract) {
+    this.call = async function (data, tag) {
 
+        if (!tag) {
+            tag = 'latest';
+        }
+        if (tag !== 'latest' && tag !== 'earliest' && tag !== 'pending') {
+            throw new Error('invalid  tag:' + tag);
+        }
 
+        request.method = 'eth_call';
+        request.params = [data, tag];
+        request.id = id();
 
+        return await fetch(host, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+        }).then(res => res.json()).then(res => {
+            if (res.error) {
+                throw new Error(res.error);
+            }
+            return validHex(res.result);
+        });
 
     };
 
-    this.generateTx = async function (rawTx,privateKey) {
+    this.generateTx = async function (rawTx, privateKey) {
 
         const wallet = new PrivateKey();
         wallet.setPrivateKey(ethUtil.toBuffer(privateKey));
@@ -146,7 +167,7 @@ function relay(host) {
 
         console.log(JSON.stringify(request));
 
-        return await fetch(host,{
+        return await fetch(host, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -161,13 +182,172 @@ function relay(host) {
 
     };
 
+    this.getTokenBalance = async function (token, add, tag) {
+
+        if (!validataor.isValidETHAddress(add)) {
+            throw new Error('invalid ETH address');
+        }
+
+        if (!validataor.isValidETHAddress(token)) {
+
+            throw new Error('invalid token contract Address');
+        }
+        const method = '0x' + ethUtil.sha3('balanceOf(address)').toString('hex').slice(0, 8);
+        const value = ethUtil.setLengthLeft(ethUtil.toBuffer(add), 32).toString('hex');
+        const data = method + value;
+
+        const params = {
+            to: token,
+            data
+        };
+
+        if (!tag) {
+            tag = 'latest';
+        }
+
+        if (tag !== 'latest' && tag !== 'earliest' && tag !== 'pending') {
+            throw new Error('invalid  tag:' + tag);
+        }
+        return new BigNumber(Number(await this.call(params, tag)));
+
+    };
+
+    this.getTokenAllowance = async function (token, owner, spender, tag) {
+
+        if (!validataor.isValidETHAddress(owner)) {
+            throw new Error('invalid owner address');
+        }
+
+        if (!validataor.isValidETHAddress(spender)) {
+            throw new Error('invalid spender address');
+        }
+
+        if (!validataor.isValidETHAddress(token)) {
+
+            throw new Error('invalid token Contract Address');
+        }
+
+        const method = '0x' + ethUtil.sha3('allowance(address,address)').toString('hex').slice(0, 8);
+
+        const value = ethUtil.setLengthLeft(ethUtil.toBuffer(owner), 32).toString('hex') + ethUtil.setLengthLeft(ethUtil.toBuffer(spender), 32).toString('hex');
+
+        const data = method + value;
+        const params = {
+            to: token,
+            data
+        };
+
+        if (!tag) {
+            tag = 'latest';
+        }
+
+        if (tag !== 'latest' && tag !== 'earliest' && tag !== 'pending') {
+            throw new Error('invalid  tag:' + tag);
+        }
+
+        return new BigNumber(Number(await this.call(params, tag)));
+
+    };
+
+    this.setTokenAllowance = async function (token, spender, value, privateKey, gasLimit, gasPrice) {
+
+        if (!validataor.isValidETHAddress(spender)) {
+            throw new Error('invalid spender address');
+        }
+
+        if (!validataor.isValidETHAddress(token)) {
+
+            throw new Error('invalid token Contract Address');
+        }
+
+        if(_.isNumber(value)){
+
+            value = '0x' + value.toString(16);
+        }
+
+        const method = '0x' + ethUtil.sha3('approve(address,uint)').toString('hex').slice(0, 8);
+        const param = ethUtil.setLengthLeft(ethUtil.toBuffer(spender), 32).toString('hex') + ethUtil.setLengthLeft(ethUtil.toBuffer(value), 32).toString('hex');
+
+        const data = method + param;
+
+        if(_.isNumber(gasPrice)){
+            gasPrice = '0x' + gasPrice.toString(16);
+        }
+
+        if(_.isNumber(gasLimit)){
+            gasLimit = '0x' + gasLimit.toString(16);
+        }
 
 
 
+        const tx = {
+            gasPrice,
+            gasLimit,
+            to: token,
+            value: '0x0',
+            data
+        };
+
+        const rawtx = await this.generateTx(tx, privateKey);
+
+        await this.sendSignedTx(rawtx.signedTx);
+    };
+
+    this.transferToken = async function (privateKey, to, token, value, gasLimit, gasPrice) {
+
+        if (!validataor.isValidETHAddress(to)) {
+            throw new Error('invalid spender address');
+        }
+
+        if (!validataor.isValidETHAddress(token)) {
+
+            throw new Error('invalid token Contract Address');
+        }
+
+        if(_.isNumber(value)){
+
+            value = '0x' + value.toString(16);
+        }
+
+        const method = '0x' + ethUtil.sha3('transfer(address,uint)').toString('hex').slice(0, 8);
+        const params = ethUtil.setLengthLeft(ethUtil.toBuffer(to), 32).toString('hex') + ethUtil.setLengthLeft(ethUtil.toBuffer(value), 32).toString('hex');
+
+        const data = method + params;
+
+        if(_.isNumber(gasPrice)){
+            gasPrice = '0x' + gasPrice.toString(16);
+        }
+
+        if(_.isNumber(gasLimit)){
+            gasLimit = '0x' + gasLimit.toString(16);
+        }
+
+        const rawtx = {
+            gasLimit,
+            gasPrice,
+            to: token,
+            value: '0x0',
+            data
+        };
+
+        const tx = await this.generateTx(rawtx, privateKey);
+
+        await  this.sendSignedTx(tx.signedTx)
+
+    };
 
     function id() {
         return crypto.randomBytes(16).toString('hex');
-    }
+    };
+
+    function validHex(data) {
+
+        if (data === '0x') {
+            data = '0x0';
+        }
+
+        return data;
+    };
 
 }
 
